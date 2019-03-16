2d315e111ecc735fc286745c4ecf7f733f3ea0c2
tryingpfq <tryingpfq@163.com>
2019-03-16 23:44:22 +0800

    redis

--- src/main/java/com/tryingpfq/coffeehouse/CoffeeehouseApplication.java
+++ src/main/java/com/tryingpfq/coffeehouse/CoffeeehouseApplication.java
@@ -9,16 +9,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -30,15 +37,33 @@ public class CoffeeehouseApplication implements ApplicationRunner {
 	@Autowired
 	private CoffeeOrderRepository coffeeOrderRepository;
 
+	@Autowired
+	private JedisPool jedisPool;
+
+	@Autowired
+	private JedisPoolConfig jedisPoolConfig;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeehouseApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties("redis")
+	public JedisPoolConfig jedisPoolConfig(){
+		return new JedisPoolConfig();
+	}
+
+	@Bean(destroyMethod = "close")
+	public JedisPool jedisPool(@Value("${redis.host}") String host){
+		return new JedisPool(jedisPoolConfig(),host);
 	}
 
 	@Override
 	@Transactional
 	public void run(ApplicationArguments args) throws Exception {
 		//initOrders();
-		findOrders();
+		//findOrders();
+		redisTest();
 	}
 
 	public void initOrders(){
@@ -90,8 +115,29 @@ public class CoffeeehouseApplication implements ApplicationRunner {
 		log.info("findByItems_Name: {}", getJoinedOrderId(list));
 	}
 
+	private void redisTest(){
+		log.info(jedisPoolConfig.toString());
+
+		try(Jedis jedis = jedisPool.getResource()){
+			coffeeRepository.findAll().forEach(c -> {
+				jedis.hset("coffeeMenu",c.getName(),Long.toString(c.getPrice().getAmountMinorLong()));
+			});
+
+			Map<String,String> menu = jedis.hgetAll("coffeeMenu");
+			log.info("Menu:{}",menu);
+
+			String price = jedis.hget("coffeeMenu","latte");
+			log.info("latte price {}",price);
+		}
+
+
+	}
+
+
 	private String getJoinedOrderId(List<CoffeeOrder> list) {
 		return list.stream().map(o -> o.getId().toString())
 				.collect(Collectors.joining(","));
 	}
+
+
 }
